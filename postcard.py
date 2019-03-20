90a40133a74466c198dcbfd633490138e4297198
Bryce Harrington <bryce@bryceharrington.org>
2019-03-19 19:14:16 -0700

    Also allow setting label by the label name or color
    Users will want to specify the labels for their card by name (or color
    if they haven't set up name) rather than having to figure out the label
    ID.

--- postcard.py
+++ postcard.py
@@ -8,6 +8,45 @@ def load_yaml(filename):
         data = yaml.safe_load(f)
     return data
 
+def get_id_for_label(label, acceptable_labels):
+    '''Checks label is a valid name, color, or label id.
+
+    @return The label's id.
+    '''
+    assert(type(label) is str)
+    assert(type(acceptable_labels) is list)
+
+    for a in acceptable_labels:
+        if (a['name'].lower() == label.lower() or
+            a['color'].lower() == label.lower() or
+            a['id'].lower() == label.lower()):
+            return a['id']
+    return None
+
+def validate_labels(labels, acceptable_labels):
+    '''Looks up ids for raw list of labels.
+
+    @return Tuple of (valid label ids, invalid labels).
+    '''
+    card_label_ids = []
+    invalid_labels = []
+    for label in labels:
+        if ',' in label:
+            # Bit of recursion to handle comma-separated labels
+            print(label.split(','))
+            good,bad = validate_labels(label.split(','), acceptable_labels)
+            card_label_ids.extend(good)
+            invalid_labels.extend(bad)
+        else:
+            # Just a regular label
+            label_id = get_id_for_label(label, board_labels)
+            print("id for %s is %s" %(label, label_id))
+            if not label_id:
+                invalid_labels.append(label)
+            else:
+                card_label_ids.append(label_id)
+    return card_label_ids, invalid_labels
+
 if __name__ == "__main__":
     import os.path
     import sys
@@ -50,17 +89,24 @@ if __name__ == "__main__":
     trello = Trello(config['api_key'], config['oauth_token'])
 
     # Display
-    print("Board:")
+    #print("Board:")
     board = trello.get_board(BOARD)
-    pp.pprint(board)
+    #pp.pprint(board)
 
     print("Board labels:")
     board_labels = trello.get_board_labels(BOARD)
     pp.pprint(board_labels)
+    card_label_ids, invalid_labels = validate_labels(args.labels, board_labels)
+    print("Invalid labels:", invalid_labels)
+    print("Valid label ids:", card_label_ids)
+    if len(invalid_labels)>0:
+        sys.stderr.write("Error: Invalid label(s) %s specified\n" %(
+            ','.join(invalid_labels)))
+        sys.exit(1)
 
-    print("\nBoard Lists:")
+    #print("\nBoard Lists:")
     board_lists = trello.get_board_lists(BOARD)
-    pp.pprint(board_lists)
+    #pp.pprint(board_lists)
 
     print("\nSelected list:")
     if (args.column < 1) or args.column > len(board_lists)-1:
@@ -77,6 +123,5 @@ if __name__ == "__main__":
         "A New Card",
         desc="This is a description for our new card.",
         pos="bottom",
-        labels=",".join(args.labels))
+        labels=",".join(card_label_ids))
     pp.pprint(card)
-

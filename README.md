2494d8f3f2e4dddfb54f2338e7924307a907ae6d
Bryce Harrington <bryce@bryceharrington.org>
2019-03-21 18:17:43 -0700

    Handle OAuth
    Set up the oauth request, and prompt the user to load in browser to
    handle the process.  The oauth token is cached in ~/.config/postcard so
    we shouldn't need to re-authenticate every call, but will need to once
    the token has expired (every 30 days).

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 Posts cards to Trello.com
 
-To generate an oauth token, visit this URL:
-
-  https://trello.com/1/authorize?expiration=30days&name=MyPersonalToken&scope=read,write&response_type=token&key={YourAPIKey}
-
-Then click the Allow button
+Prerequisites: python3-requests-oauthlib
 
 
 ## Testing
---
+++ postcard/auth.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from requests_oauthlib import OAuth1Session
+
+# The Trello API supports basic OAuth 1.0; you can use an OAuth library and the following URLs:
+#
+# https://trello.com/1/OAuthGetRequestToken
+# https://trello.com/1/OAuthAuthorizeToken
+# https://trello.com/1/OAuthGetAccessToken
+
+def get_access_token(api_key, api_secret, scope, app_name):
+    # Get the request token
+    session = OAuth1Session(client_key=api_key, client_secret=api_secret)
+    response = session.fetch_request_token('https://trello.com/1/OAuthGetRequestToken')
+    token_key = response.get('oauth_token')
+    token_secret = response.get('oauth_token_secret')
+
+    # Redirect to Trello.com for OAuth token
+    print("Open this link in your browser to authorize access to your Trello board:\n")
+    print("%s?oauth_token=%s&scope=read,write&expiration=30days&name=%s" %(
+        "https://trello.com/1/OAuthAuthorizeToken",
+        token_key,
+        app_name,
+        ))
+
+    # Read the verification code from the user
+    reading = True
+    while reading:
+        try:
+            code = input('\n\nEnter the verification code from Trello: ')
+            reading = False
+        except:
+            print('Invalid entry')
+            reading = True
+
+    # Get the access token for the user
+    session = OAuth1Session(
+        client_key=api_key,
+        client_secret=api_secret,
+        resource_owner_key=token_key,
+        resource_owner_secret=token_secret,
+        verifier=code)
+    return session.fetch_access_token('https://trello.com/1/OAuthGetAccessToken')
+
+
+if __name__ == "__main__":
+    API_KEY = '8c3667cb97137b330d237f43ffd96c47'
+    SECRET  = 'abd1aed16d4b645ea658de620f8e7a9b133c512390e7bfdc2125e59a84936c16'
+    SCOPE   = 'read,write'
+    NAME    = 'postcard'
+
+    access_token = get_access_token(API_KEY, SECRET, SCOPE, NAME)
+    print("Access token: %s" %(access_token))
